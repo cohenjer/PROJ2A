6b913fb04be8ee06172ded817ce3f178fb1ab7d4
jecohen <jeremy.cohen@irisa.fr>
2018-11-06 14:58:59 +0100

    Derniers changements

--- reseau_neuronne_2.m
+++ reseau_neuronne_2.m
@@ -10,28 +10,23 @@ clc
 % grille sur [0,1]
 
 % Nombre de points sur la grille
-Nb = 50;
+Nb = 6;
 
 %xi = rand(1,Nb);
 xi = linspace(0,1,Nb);
 
-% Test data
-X = xi(1,1:2:end-1);
-X = [X;ones(1,Nb/2)];
-
 % Train data
-Xt = xi(2:2:end);
-Xt = [Xt;ones(1,Nb/2)];
+Xt = xi;
+Xt = [Xt;ones(1,Nb)];
 
 % Fonction carrée comme fonction que l'on veut interpoler
-yi = xi.^2;
-y = yi(1:2:end-1);
-yt = yi(2:2:end);
+yi = xi.^2+cos(10*xi);
+yt = yi;
 
 % Ici les dimensions de W relèvent du choix de l'utilisateur. Pourquoi
 % mettre 500 ? Ici avec 1 au lieu de 500, ca marche aussi...
 % J'ai également mis des randn au lieu de ones.
-dims = 10;
+dims = 100;
 W = randn(dims,2);
 W2 = randn(1,dims+1);
 
@@ -46,9 +41,6 @@ mu = 0.01;
 % Mes corrections:
 %   Corrections Majeures
 %---------------------------------
-% - Erreur dans la syntaxe pour la mise à jour de W2. W2 n'est plus un
-% vecteur colonne puisque Xt contient 2 lignes. J'ai totalement changé ce
-% morceau.
 % - Erreur aussi dans la syntaxe pour W (calculs probablement OK mais
 % implémentation bizare). J'ai changé en refaisant comme toi les calculs à
 % la main.
@@ -75,7 +67,7 @@ while (err_mean>1e-5)
     fprintf('err_mean: %d\n',err_mean)
     err_mean = 0;
     
-    index_list = randperm(length(xi)/2);
+    index_list = randperm(length(xi));
     
     for i=index_list
     
@@ -84,18 +76,9 @@ while (err_mean>1e-5)
     err_mean = err_mean + err^2;
 
     
-    %for k=1:M
-    %    for j=1:N
-    %    W2(k,j) = W2(k,j) - mu*err*a1;
-    %    end
-    %end
     W2_old = W2;
     W2 = W2 - mu*err*[a1;1]'; 
     
-   
-    % Ta version, incorrecte? (Xt(k)? a2(1-a2)?)    
-    %W(k,j) = W(k,j) - mu*(a2-Xt(k)^2)*a2*(1-a2)*W2(k)*a1(k)*(1-a1(k))*Xt(j,k); 
-            
             % Version correcte loop
     %    for j=1:N
     %        for k=1:M
@@ -122,32 +105,19 @@ for w=1:length(Xt)
 end
 
 
-% Sorties y_est sur les données test
-for w=1:length(X)
-    [~,~,out] =NNforward(X(:,w),W,W2);
-    y_est(1,w)= out;
-end
-
 % Tracés
     % 1/ Performances sur données train (normalement très bon)
     figure
-    plot(Xt(1,:),yt,'--r')
+    plot(Xt(1,:),yt,'+')
     hold on
-    plot(Xt(1,:),yt_est,'k')
+    plot(Xt(1,:),yt_est,'*')
     legend('True','NN')
     title('Train')
         
-    % 2/ Performances sur données test
-    figure    
-    plot(X(1,:),y,'--r')
-    hold on
-    plot(X(1,:),y_est,'k')
-    legend('True','NN')
-    title('Test')
     
-    % 3/ Sur une grille fine
-    grille = linspace(-0.2,1.2,1000);
-    fun_real = grille.^2;
+    % 3/ Sur une grille fine (test)
+    grille = linspace(0,1,1000);
+    fun_real = grille.^2 + cos(10*grille);
     for l=1:1000
        [~,~, toto] = NNforward([grille(l);1],W,W2);
        fun_est(l) = toto;
